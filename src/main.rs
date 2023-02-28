4cb8e89bfa108a29efd85e40ceb2cd5e4ef6aea1
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2023-02-28 23:00:14 +0100

    feat: add csv to main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
+mod csv;
 mod sender;
 
-fn main() {
-    let target = "192.168.55.100:8210";
+use crate::csv::Entry;
 
-    match sender::send_entries(target) {
-        Ok(()) => println!("Entries imported successfully."),
-        Err(e) => eprintln!("Failed to import entries\n\nError: {}", e),
-    };
+fn main() {
+
+    // Define input variabes - later user input via cli
+    let target: &str = "192.168.55.100:8210";
+    let filepath: &str = "/home/dstrobel/Documents/entries.csv";
+
+    let mut entries: Vec<Entry> = Vec::new();
+
+    // Get entries from CSV
+    match csv::get_csv_entries(filepath) {
+        Ok(parsed_entries) => {
+            entries = parsed_entries;
+        },
+        Err(e) => eprintln!("Failed to parse entries from CSV file {}\nError: {}", filepath, e),
+    }
+
+    // Get entries from CSV
+    match sender::send_entries(target, entries) {
+        Ok(()) => println!("Successfully imported entries to DSP {}", target),
+        Err(e) => eprintln!("Failed to import entries to DSP {}\nError: {}", target, e),
+    }
 }
