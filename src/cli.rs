9dc622c67ac73e774bd293cd460baca332357da6
d-strobel <github.6f1aa@slmails.com>
2025-04-24 22:45:10 +0200

    feat: add shell completion with subcommand

--- src/cli.rs
+++ src/cli.rs
@@ -1,29 +1,45 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use clap_complete::Shell;
 use std::path::PathBuf;
 
 /// Import timecode timestamps and entries to Lasergraph DSP
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    /// IP-Address of the Lasergraph DSP
-    #[arg(short, long, value_name = "IP-ADDRESS")]
-    pub address: String,
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
 
-    /// TCP/IP port of the lasergraph DSP for remoting
-    #[arg(short, long, default_value_t = 8210)]
-    pub port: i32,
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    /// Generate shell completions
+    Completions {
+        #[arg(value_enum)]
+        shell: Shell,
+    },
 
-    /// Path to the CSV-file
-    #[arg(short, long, value_name = "FILE")]
-    pub csv: PathBuf,
+    /// Run the CSV importer
+    Import {
+        /// IP-Address of the Lasergraph DSP
+        #[arg(short, long, value_name = "IP-ADDRESS")]
+        address: String,
 
-    /// Defines if entries should be created
-    #[arg(short = 'e', long)]
-    pub create_entry: bool,
+        /// TCP/IP port of the lasergraph DSP for remoting
+        #[arg(short, long, default_value_t = 8210)]
+        port: i32,
 
-    /// Defines the number of the first Entry
-    #[arg(short, long, default_value_t = 0, value_name = "ENTRY")]
-    pub start_entry: i32,
+        /// Path to the CSV-file
+        #[arg(short, long, value_name = "FILE")]
+        csv: PathBuf,
+
+        /// Defines if entries should be created
+        #[arg(short = 'e', long)]
+        create_entry: bool,
+
+        /// Defines the number of the first Entry
+        #[arg(short, long, default_value_t = 0, value_name = "ENTRY")]
+        start_entry: i32,
+    },
 }
 
 #[cfg(test)]
@@ -32,69 +48,67 @@ mod tests {
 
     #[test]
     fn test_parse_args() {
-        // Test with only neccessary args
-        let args = Args::parse_from(&[
-            "program_name",
+        // Test importer with only neccessary args
+        let args = Args::parse_from([
+            "dummy", //argv[0] will be ignored
+            "import",
             "--address",
             "127.0.0.1",
             "--csv",
             "/path/to/file.csv",
         ]);
 
-        assert_eq!(args.address, "127.0.0.1");
-        assert_eq!(args.csv.to_str(), Some("/path/to/file.csv"));
-        assert_eq!(args.port, 8210);
-        assert_eq!(args.create_entry, false);
-        assert_eq!(args.start_entry, 0);
+        match args.command {
+            Some(Commands::Import {
+                address,
+                port,
+                csv,
+                create_entry,
+                start_entry,
+            }) => {
+                assert_eq!(address, "127.0.0.1");
+                assert_eq!(csv.to_str(), Some("/path/to/file.csv"));
+                assert_eq!(port, 8210);
+                assert!(!create_entry);
+                assert_eq!(start_entry, 0);
+            }
+            _ => {
+                panic!("Expteced 'import' command")
+            }
+        }
 
-        // Test with port
-        let args = Args::parse_from(&[
-            "program_name",
+        // Test importer with all arguments
+        let args = Args::parse_from([
+            "dummy", //argv[0] will be ignored
+            "import",
             "--address",
             "127.0.0.1",
             "--csv",
             "/path/to/file.csv",
             "--port",
             "8000",
-        ]);
-
-        assert_eq!(args.address, "127.0.0.1");
-        assert_eq!(args.csv.to_str(), Some("/path/to/file.csv"));
-        assert_eq!(args.port, 8000);
-        assert_eq!(args.create_entry, false);
-        assert_eq!(args.start_entry, 0);
-
-        // Test with start_entry
-        let args = Args::parse_from(&[
-            "program_name",
-            "--address",
-            "127.0.0.1",
-            "--csv",
-            "/path/to/file.csv",
             "--start-entry",
             "50",
-        ]);
-
-        assert_eq!(args.address, "127.0.0.1");
-        assert_eq!(args.csv.to_str(), Some("/path/to/file.csv"));
-        assert_eq!(args.port, 8210);
-        assert_eq!(args.create_entry, false);
-        assert_eq!(args.start_entry, 50);
-
-        // Test with create_entry
-        let args = Args::parse_from(&[
-            "program_name",
-            "--address",
-            "127.0.0.1",
-            "--csv",
-            "/path/to/file.csv",
             "--create-entry",
         ]);
 
-        assert_eq!(args.address, "127.0.0.1");
-        assert_eq!(args.csv.to_str(), Some("/path/to/file.csv"));
-        assert_eq!(args.port, 8210);
-        assert_eq!(args.create_entry, true);
-        assert_eq!(args.start_entry, 0);
+        match args.command {
+            Some(Commands::Import {
+                address,
+                port,
+                csv,
+                create_entry,
+                start_entry,
+            }) => {
+                assert_eq!(address, "127.0.0.1");
+                assert_eq!(csv.to_str(), Some("/path/to/file.csv"));
+                assert_eq!(port, 8000);
+                assert!(create_entry);
+                assert_eq!(start_entry, 50);
+            }
+            _ => {
+                panic!("Expected 'import' command")
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod error;
 mod sender;
 
 use crate::csv::Entry;
-use clap::Parser;
+use clap::{CommandFactory, Parser};
 use log::{error, info};
 use std::path::PathBuf;
 
@@ -17,25 +17,49 @@ fn main() {
     // Parse arguments from CLI
     let args = cli::Args::parse();
 
-    let target: String = format!("{}:{}", args.address, args.port);
-    let filepath: PathBuf = args.csv;
-    let entry_offset: i32 = args.start_entry;
-
-    // Get entries from CSV
-    let entries: Vec<Entry> = match csv::get_csv_entries(filepath) {
-        Ok(parsed_entries) => parsed_entries,
-        Err(e) => {
-            error!("Failed to parse entries from CSV file with error: {}", e);
-            std::process::exit(1);
+    match args.command {
+        // Handle shell completions
+        Some(cli::Commands::Completions { shell }) => {
+            let mut cmd = cli::Args::command();
+            let name = cmd.get_name().to_string();
+            clap_complete::generate(shell, &mut cmd, name, &mut std::io::stdout());
         }
-    };
 
-    // Send entries to DSP
-    match sender::send(&target, entries, args.create_entry, entry_offset) {
-        Ok(()) => println!("Successfully sent entries to DSP {}", target),
-        Err(e) => {
-            error!("Failed to send entries to DSP with error: {}", e);
-            std::process::exit(1);
+        // Run the importer
+        Some(cli::Commands::Import {
+            address,
+            port,
+            csv,
+            create_entry,
+            start_entry,
+        }) => {
+            let target: String = format!("{}:{}", address, port);
+            let filepath: PathBuf = csv;
+            let entry_offset: i32 = start_entry;
+
+            // Get entries from CSV
+            let entries: Vec<Entry> = match csv::get_csv_entries(filepath) {
+                Ok(parsed_entries) => parsed_entries,
+                Err(e) => {
+                    error!("Failed to parse entries from CSV file with error: {}", e);
+                    std::process::exit(1);
+                }
+            };
+
+            // Send entries to DSP
+            match sender::send(&target, entries, create_entry, entry_offset) {
+                Ok(()) => println!("Successfully sent entries to DSP {}", target),
+                Err(e) => {
+                    error!("Failed to send entries to DSP with error: {}", e);
+                    std::process::exit(1);
+                }
+            };
         }
-    };
+
+        // Show help if no subcommand is provided
+        None => {
+            cli::Args::command().print_help().unwrap();
+            std::process::exit(0);
+        }
+    }
 }
